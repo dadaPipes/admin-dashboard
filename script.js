9ca2884719f00943aca45ef3e4d8068ef8633231
dadaPipes <markoliverstockflethstaf@gmail.com>
2023-11-04 00:25:19 +0100

    Make layout

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('searchButton').addEventListener('click', function() {
+    var searchQuery = document.getElementById('searchInput').value;
+    // Perform your search operation with searchQuery
+    console.log('Searching for: ' + searchQuery);
+  });
